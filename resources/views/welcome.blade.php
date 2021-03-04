09b74716f5577b9b4c2f59a44abfbab4502c596f
NITESHDHIMAL <niteshdhimal18@gmail.com>
2021-03-04 08:32:05 +0545

    Hello world

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        <style type="text/css">
+             h1{
+                font-size: 20px;
+                color: red;
+             }           
+ 
+
+        </style>
+
+
+    </title>
+</head>
+<body>
+
+<h1>Hellow World</h1>
+
+
+</body>
+</html>
